e62d2a3fc8de3531d471107bf7c182dfa66e38a7
Sreejith.k <sreejith.k@bytestrone.com>
2026-08-17 17:35:46 +0530

    chore: remove local session resume helper scripts

--- resume.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-REM Quick script to resume the last Wanderly session in AGY with full context
-cd /d "E:\shanu.sp\Travel website"
-echo Resuming your latest AGY session...
-agy --continue
